226a8400646c0322c3e1fb0a01cab598c5ebf908
Evgeny Berezhnov <jackberezhnov@gmail.com>
2019-08-08 21:32:23 -0700

    fix span element

--- src/text.js
+++ src/text.js
@@ -2,8 +2,3 @@ var typed = new Typed('#typed-string', {
     strings: ["Web developer", "<h1>Web developer</h1>", "<h1>Frontend developer</h1>" ],
     typeSpeed: 50
 });
-
-function hideme() {
-    var span = document.getElementsByClassName("typed-cursor");
-    span.parentNode.removeChild(span);
-}
