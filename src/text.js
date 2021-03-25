9eac9eb140822dcb77e3b0086c8a42f3357e7e39
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2021-03-25 10:52:08 -0700

    Added Calculator and updated resume

--- src/text.js
+++ src/text.js
@@ -1,4 +1,4 @@
-var typed = new Typed('#typed-string', {
+let typed = new Typed('#typed-string', {
     strings: ["Web developer", "<h1>Web developer</h1>", "<h1>Frontend developer</h1>" ],
     typeSpeed: 50
 });
