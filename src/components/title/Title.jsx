2d92e3093c9008e8eb2284d96ce75b1465dbb0ca
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2022-02-17 09:58:39 -0800

    Styled buttons

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -4,6 +4,10 @@ function Title() {
     return(
         <div class="title">
             <h1><span className="blue">Hi!</span> I am<br/> Evgeny Berezhnov <br/><span className="blue">Frontend developer</span></h1>
+            <div className='tilte-buttons'>
+                <a href="#" className='cv-btn'>resume</a>
+                <a href="#" className='contact-btn'>contact</a>
+            </div>
         </div>
     );
 }
