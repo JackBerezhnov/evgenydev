7214874bef5b13702b0dc5bb6ed9f48c27700bb9
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2022-02-17 20:48:31 -0800

    created InfoCard component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,13 @@
 import './About.css';
+import InfoCard from '../info-card/InfoCard';
 
 function About() {
     return(
         <div class="about container">
             <h2>About Me</h2>
+            <div class="description">
+                <InfoCard />
+            </div>
         </div>
     );
 }
---
+++ src/components/info-card/InfoCard.jsx
@@ -0,0 +1,9 @@
+function InfoCard() {
+    return(
+        <div className="info-card">
+            <h3>Card</h3>
+        </div>
+    );
+}
+
+export default InfoCard;
