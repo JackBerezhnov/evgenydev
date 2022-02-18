6e8b3939d3641b7a61f6a2c9c64f1a352af0d5ba
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2022-02-17 20:11:00 -0800

    Cread About component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/header/Header';
+import About from './components/about/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import './About.css';
+
+function About() {
+    return(
+        <div class="about container">
+            <h2>About Me</h2>
+        </div>
+    );
+}
+
+export default About;
