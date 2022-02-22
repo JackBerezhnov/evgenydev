818dbe721bf21e45e05830d075e3f0ab9731096e
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2022-02-21 21:04:20 -0800

    Added Roboto Mono font

--- src/components/info-card/InfoCard.jsx
+++ src/components/info-card/InfoCard.jsx
@@ -5,14 +5,14 @@ function InfoCard({imagePath}) {
     return(
         <div className="info-card">
             <img className="avatar" src={imagePath} alt="avatar"/>
-            <div className="info-description">
-                <ul>
+            <div className="info-description-container">
+                <ul className="info-description">
                     <li>Name: Evgeny Berezhnov</li>
                     <li>Phone: +1(858)225-9980</li>
                     <li>Email: jackberezhnov@gmail.com</li>
                     <li>
-                        <Icon icon="ant-design:github-filled" />
-                        <Icon icon="akar-icons:linkedin-fill" />
+                        <a href="https://github.com/JackBerezhnov" target="_blank" rel="noreferrer"><Icon icon="ant-design:github-filled" /></a>
+                        <a href="https://www.linkedin.com/in/evgeny-berezhnov-6b6178174/" target="_blank" rel="noreferrer"><Icon icon="akar-icons:linkedin-fill" /></a>
                     </li>
                 </ul>
             </div>
