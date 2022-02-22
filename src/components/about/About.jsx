baaedf57c0c6d6a214193c3ec8b24350fb28da5b
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2022-02-21 21:24:07 -0800

    added descrption about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,6 +8,7 @@ function About() {
             <h2>About Me</h2>
             <div className="description">
                 <InfoCard imagePath={avatar}/>
+                <p>My name is Evgeny Berezhnov. I'm Web Developer at San Diego Mesa College and at Core Systems. Also, I'm part-time student. My major is Computer Science. I like programming and was on the CruzHacks and SFhacks with my friends we created something cool together. I live in San Diego, California.</p>
             </div>
         </div>
     );
