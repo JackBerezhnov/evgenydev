95500e719cc62d1409c63ae46946863bff1aa3ac
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2022-02-21 20:54:24 -0800

    Added Iconify library

--- src/components/info-card/InfoCard.jsx
+++ src/components/info-card/InfoCard.jsx
@@ -1,9 +1,21 @@
 import "./InfoCard.css";
+import { Icon } from '@iconify/react';
 
 function InfoCard({imagePath}) {
     return(
         <div className="info-card">
             <img className="avatar" src={imagePath} alt="avatar"/>
+            <div className="info-description">
+                <ul>
+                    <li>Name: Evgeny Berezhnov</li>
+                    <li>Phone: +1(858)225-9980</li>
+                    <li>Email: jackberezhnov@gmail.com</li>
+                    <li>
+                        <Icon icon="ant-design:github-filled" />
+                        <Icon icon="akar-icons:linkedin-fill" />
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
